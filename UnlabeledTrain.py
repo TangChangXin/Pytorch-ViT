ecead78813f705700427fad7b35fa4bcd88870cb
TangChangXin <592609672@qq.com>
2022-01-12 15:16:35 +0800

    无标签数据训练

--- UnlabeledTrain.py
+++ UnlabeledTrain.py
@@ -14,15 +14,25 @@ from VisionTransformerModel import vit_base_patch16_224_in21k as create_model
 from utils import read_split_data, train_one_epoch, evaluate
 
 
-def main(args):
-    device = torch.device(args.device if torch.cuda.is_available() else "cpu")
+def main(命令行参数):
+    # 设置训练使用的设备
+    if torch.cuda.is_available():
+        硬件设备 = torch.device("cuda:0")
+        # 保证每次返回的卷积算法将是确定的，如果配合上设置 Torch 的随机种子为固定值的话，应该可以保证每次运行网络的时候相同输入的输出是固定的。
+        torch.backends.cudnn.deterministic = True
+        if torch.backends.cudnn.deterministic:
+            print("确定卷积算法")
+        torch.backends.cudnn.benchmark = False  # 为每层搜索适合的卷积算法实现，加速计算
+    else:
+        硬件设备 = torch.device("cpu")
+    print("训练使用设备", 硬件设备)
 
     if os.path.exists("./weights") is False:
         os.makedirs("./weights")
 
     tb_writer = SummaryWriter()
 
-    train_images_path, train_images_label, val_images_path, val_images_label = read_split_data(args.data_path)
+    train_images_path, train_images_label, val_images_path, val_images_label = read_split_data(命令行参数.data_path)
 
     data_transform = {
         "train": transforms.Compose([transforms.RandomResizedCrop(224),
@@ -44,18 +54,18 @@ def main(args):
                             images_class=val_images_label,
                             transform=data_transform["val"])
 
-    batch_size = args.batch_size
+    batch_size = 命令行参数.batch_size
     nw = min([os.cpu_count(), batch_size if batch_size > 1 else 0, 8])  # number of workers
     print('Using {} dataloader workers every process'.format(nw))
     train_loader = torch.utils.data.DataLoader(train_dataset,
-                                               batch_size=batch_size,
+                                               batch_size=unlabeled_data_batch_size,
                                                shuffle=True,
                                                pin_memory=True,
                                                num_workers=nw,
                                                collate_fn=train_dataset.collate_fn)
 
     val_loader = torch.utils.data.DataLoader(val_dataset,
-                                             batch_size=batch_size,
+                                             batch_size=unlabeled_data_batch_size,
                                              shuffle=False,
                                              pin_memory=True,
                                              num_workers=nw,
@@ -63,9 +73,9 @@ def main(args):
 
     model = create_model(num_classes=5, has_logits=False).to(device)
 
-    if args.weights != "":
-        assert os.path.exists(args.weights), "weights file: '{}' not exist.".format(args.weights)
-        weights_dict = torch.load(args.weights, map_location=device)
+    if 命令行参数.weights != "":
+        assert os.path.exists(命令行参数.weights), "weights file: '{}' not exist.".format(命令行参数.weights)
+        weights_dict = torch.load(命令行参数.weights, map_location=device)
         # 删除不需要的权重
         del_keys = ['head.weight', 'head.bias'] if model.has_logits \
             else ['pre_logits.fc.weight', 'pre_logits.fc.bias', 'head.weight', 'head.bias']
@@ -73,7 +83,7 @@ def main(args):
             del weights_dict[k]
         print(model.load_state_dict(weights_dict, strict=False))
 
-    if args.freeze_layers:
+    if 命令行参数.freeze_layers:
         for name, para in model.named_parameters():
             # 除head, pre_logits外，其他权重全部冻结
             if "head" not in name and "pre_logits" not in name:
@@ -82,12 +92,12 @@ def main(args):
                 print("training {}".format(name))
 
     pg = [p for p in model.parameters() if p.requires_grad]
-    optimizer = optim.SGD(pg, lr=args.lr, momentum=0.9, weight_decay=5E-5)
+    optimizer = optim.SGD(pg, lr=命令行参数.lr, momentum=0.9, weight_decay=5E-5)
     # Scheduler https://arxiv.org/pdf/1812.01187.pdf
-    lf = lambda x: ((1 + math.cos(x * math.pi / args.epochs)) / 2) * (1 - args.lrf) + args.lrf  # cosine
+    lf = lambda x: ((1 + math.cos(x * math.pi / 命令行参数.epochs)) / 2) * (1 - 命令行参数.lrf) + 命令行参数.lrf  # cosine
     scheduler = lr_scheduler.LambdaLR(optimizer, lr_lambda=lf)
 
-    for epoch in range(args.epochs):
+    for epoch in range(命令行参数.epochs):
         # train
         train_loss, train_acc = train_one_epoch(model=model,
                                                 optimizer=optimizer,
@@ -114,26 +124,27 @@ def main(args):
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--num_classes', type=int, default=5)
-    parser.add_argument('--epochs', type=int, default=10)
-    parser.add_argument('--batch-size', type=int, default=8)
-    parser.add_argument('--lr', type=float, default=0.001)
-    parser.add_argument('--lrf', type=float, default=0.01)
+    命令行参数解析器 = argparse.ArgumentParser()
+    命令行参数解析器.add_argument('--num_classes', type=int, default=5)
+    命令行参数解析器.add_argument('--epochs', type=int, default=10)
+    命令行参数解析器.add_argument('--batch_size', type=int, default=8)
+    命令行参数解析器.add_argument('--unlabeled_data_batch_size', type=int, default=3, help='')
+    命令行参数解析器.add_argument('--lr', type=float, default=0.001)
+    命令行参数解析器.add_argument('--lrf', type=float, default=0.01)
 
     # 数据集所在根目录
     # http://download.tensorflow.org/example_images/flower_photos.tgz
-    parser.add_argument('--data-path', type=str,
+    命令行参数解析器.add_argument('--data-path', type=str,
                         default="/data/flower_photos")
-    parser.add_argument('--model-name', default='', help='create model name')
+    命令行参数解析器.add_argument('--model-name', default='', help='create model name')
 
     # 预训练权重路径，如果不想载入就设置为空字符
-    parser.add_argument('--weights', type=str, default='./vit_base_patch16_224_in21k.pth',
+    命令行参数解析器.add_argument('--weights', type=str, default='./vit_base_patch16_224_in21k.pth',
                         help='initial weights path')
     # 是否冻结权重
-    parser.add_argument('--freeze-layers', type=bool, default=True)
-    parser.add_argument('--device', default='cuda:0', help='device id (i.e. 0 or 0,1 or cpu)')
+    命令行参数解析器.add_argument('--freeze-layers', type=bool, default=True)
+    命令行参数解析器.add_argument('--device', default='cuda:0', help='device id (i.e. 0 or 0,1 or cpu)')
 
-    opt = parser.parse_args()
+    opt = 命令行参数解析器.parse_args()
 
     main(opt)
